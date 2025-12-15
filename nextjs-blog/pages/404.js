bc5f31cddef4691c0700d9c543d792a090b803e1
Richard Mery <richard.mery@appius.com>
2025-12-15 17:12:42 +0000

    Minor amends

--- nextjs-blog/pages/404.js
+++ nextjs-blog/pages/404.js
@@ -1,5 +1,16 @@
 import utilStyles from '../styles/Utils.module.css';
+import styles from '../styles/Layout.module.css';
+import Link from 'next/link';
 
 export default function Custom404() {
-  return <h1 className={utilStyles.textCenter}>404 - Ooops! Something's not quite right</h1>;
+  return (
+    <>
+      <h1 className={utilStyles.textCenter}>
+        404 - Ooops! Something's not quite right.
+      </h1>
+        <div className={`${styles.backToHome} ${utilStyles.textCenter}`}>
+          <Link href="/">← Back to home</Link>
+        </div>
+    </>
+  ); 
 }
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -29,13 +29,9 @@ export async function getStaticProps() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>We dispose of stuff.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+      {/* <section className={utilStyles.headingMd}>
+        <h2>We dispose of stuff.</h2>
+      </section> */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
             <h2 className={utilStyles.headingLg}>Blog</h2>
             <ul>
