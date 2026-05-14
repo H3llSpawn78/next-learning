dedb990d144073d37779334abfd43a51b81f5b61
Richard Mery <richard.mery@appius.com>
2026-05-14 15:51:42 +0100

    props name amend for apiProps

--- RHF/react-hook-forms/app/pokemon/page.tsx
+++ RHF/react-hook-forms/app/pokemon/page.tsx
@@ -4,14 +4,14 @@ import React, { useEffect, useState } from "react";
 import PokemonList from "@/app/components/pokemon/PokemonList";
 import axios from "axios";
 
-type pokemonProps = {
-  pokemonListItems: string[];
+type pokemonAPIProps = {
+  //pokemonListItems: string[];
   name: string;
   url: string;
 };
 
 export default function Pokemon() {
-  const [pokemon, setPokemon] = useState<pokemonProps[]>([]);
+  const [pokemon, setPokemon] = useState<pokemonAPIProps[]>([]);
 
   useEffect(() => {
     axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
