6c5274025d7bef9e3a199cf318d77c59a4dd2591
Richard Mery <richard.mery@appius.com>
2026-07-02 17:49:59 +0100

    progress with props, .map() and using an external data source

--- React-Learning/scrimba/travel-journal/src/App.tsx
+++ React-Learning/scrimba/travel-journal/src/App.tsx
@@ -1,14 +1,30 @@
 import "./App.css";
 import Header from "./components/Header";
 import Entry from "./components/Entry";
+import data from "./data";
 
 function App() {
+  const entries = data.map((entry) => (
+    <Entry
+      key={entry.id}
+      img={{
+        src: entry.img.src,
+        alt: entry.img.alt,
+      }}
+      title={entry.title}
+      country={entry.country}
+      googleMapsLink={entry.googleMapsLink}
+      dates={entry.dates}
+      text={entry.text}
+    />
+  ));
+
+  //console.log(data);
   return (
     <>
       <Header />
-      <Entry />
+      <div className="container">{entries}</div>
     </>
   );
 }
-
 export default App;
--- React-Learning/scrimba/travel-journal/src/components/Entry.tsx
+++ React-Learning/scrimba/travel-journal/src/components/Entry.tsx
@@ -1,10 +1,16 @@
-export default function Entry() {
+const currentDate = new Date();
+const time = currentDate.toLocaleTimeString([], {
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+export default function Entry(props: any) {
   return (
     <article className="journal-entry">
       <div className="journal-entry__image-wrapper">
         <img
-          src="https://scrimba.com/links/travel-journal-japan-image-url"
-          alt="test image"
+          src={props.img.src}
+          alt={props.img.alt}
           className="journal-entry__image"
         />
       </div>
@@ -16,10 +22,11 @@ export default function Entry() {
               src="./src/assets/marker.svg"
               alt="marker icon"
             />
-            <h2 className="journal-entry__header-location-name">Japan</h2>
+            <h2 className="journal-entry__header-location-name">
+              {props.country}
+            </h2>
             <a
-              href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu
-"
+              href={props.googleMapsLink}
               className="journal-entry__header-location-link"
             >
               View on Google Maps
@@ -27,15 +34,11 @@ export default function Entry() {
           </span>
         </div>
         <div className="journal-entry__body">
-          <h3 className="journal-entry__body-title">Mount Fuji</h3>
+          <h3 className="journal-entry__body-title">{props.title}</h3>
           <span className="journal-entry__body-dates">
-            Date: 12 Jan, 2021 - 24 Jan, 2021
+            {props.dates} - {time}
           </span>
-          <p className="journal-entry__body-text">
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
-          </p>
+          <p className="journal-entry__body-text">{props.text}</p>
         </div>
       </div>
     </article>
