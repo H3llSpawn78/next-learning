e1c5daeda4bef876fc40a9e2aba71d816dcee586
Richard Mery <richard.mery@appius.com>
2026-07-06 11:33:01 +0100

    Added comments

--- React-Learning/scrimba/travel-journal/src/App.tsx
+++ React-Learning/scrimba/travel-journal/src/App.tsx
@@ -7,13 +7,20 @@ function App() {
   const entries = data.map((entry) => (
     <Entry
       key={entry.id}
+      // Passing in individual object props
       // img={entry.img}
       // title={entry.title}
       // country={entry.country}
       // googleMapsLink={entry.googleMapsLink}
       // dates={entry.dates}
       // text={entry.text}
+
+      // Passing in the entiore object -
+      // must append the prop decs in the component to use 'props.entry.img etc
       //entry={entry}
+
+      // Pass in the entire object using the spread operator -
+      // no need to append the dec props on the component with 'props.entry.img' etc
       {...entry}
     />
   ));
