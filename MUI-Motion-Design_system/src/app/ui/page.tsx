f6079a82c7c3df83fad37018b4395954bcff1bf7
Richard Mery <richard.mery@appius.com>
2026-08-07 15:32:40 +0100

    Amends to design system files

--- MUI-Motion-Design_system/src/app/ui/page.tsx
+++ MUI-Motion-Design_system/src/app/ui/page.tsx
@@ -6,134 +6,176 @@ import ShareIcon from "@mui/icons-material/Share";
 import Grid from "@mui/material/Grid";
 import TwoColumnLayout from "@/components/ui/TwoColLayout/TwoColLayout";
 import MediaCard from "@/components/ui/Card/AppMediaCard";
-import { FadeIn } from "@/animations/mui/FadeInBox";
+import { FadeIn } from "@/animations/FadeIn";
+import { FadeInBox } from "@/animations/mui/FadeInBox";
 import { StaggerTypography } from "@/animations/mui/StaggerTypography";
 
 export default function Page() {
-	return (
-		<>
-			<Grid container spacing={2} sx={{ marginTop: "1000px" }}>
-				<StyledFlexBox size={{ xs: 12, md: 6 }}>
-					<StaggerTypography
-						variant="h1"
-						text="This text will stagger in from the pub ;)"
-					/>
-				</StyledFlexBox>
-				<StyledFlexBox size={{ xs: 12, md: 6 }}>
-					<AppButton
-						variant="outlined"
-						color="primary"
-						endIcon={<DeleteIcon />}
-					>
-						Test button
-					</AppButton>
-				</StyledFlexBox>
-			</Grid>
-			<Grid container spacing={2}>
-				<TwoColumnLayout
-					leftColWidth={{
-						xs: "100%",
-						md: "100%",
-					}}
-					rightColWidth={{
-						xs: "100%",
-						md: "100%",
-					}}
-					leftCol={
-						<AppButton
-							variant="outlined"
-							color="primary"
-							endIcon={<DeleteIcon />}
-						>
-							Test button
-						</AppButton>
-					}
-				/>
-			</Grid>
-			<Grid container spacing={2}>
-				<TwoColumnLayout
-					leftColWidth={{
-						xs: "100%",
-						md: "100%",
-					}}
-					rightColWidth={{
-						xs: "100%",
-						md: "100%",
-					}}
-					leftCol={
-						<AppButton
-							variant="outlined"
-							color="primary"
-							endIcon={<ShareIcon />}
-						>
-							Test button
-						</AppButton>
-					}
-					rightCol={
-						<FadeIn>
-							<Typography variant="h1">This text will fade in...</Typography>
-						</FadeIn>
-					}
-				/>
-			</Grid>
-			<Grid container spacing={2}>
-				<FadeIn>
-					<TwoColumnLayout
-						leftColWidth={{
-							xs: "100%",
-							md: "100%",
-						}}
-						rightColWidth={{
-							xs: "100%",
-							md: "100%",
-						}}
-						leftCol={
-							<MediaCard
-								title="Test title"
-								variant="primary"
-								description=""
-								image="https://picsum.photos/480/240"
-								actions={
-									<>
-										<AppButton
-											variant="outlined"
-											color="primary"
-											endIcon={<DeleteIcon />}
-										>
-											Test button
-										</AppButton>
-										<AppButton color="primary" endIcon={<ShareIcon />}>
-											Learn More
-										</AppButton>
-									</>
-								}
-							/>
-						}
-						rightCol={
-							<MediaCard
-								title="Test title"
-								variant="primary"
-								description=""
-								image="https://picsum.photos/480/240"
-								actions={
-									<>
-										<AppButton
-											variant="outlined"
-											color="primary"
-											endIcon={<DeleteIcon />}
-										>
-											Test button
-										</AppButton>
-										<AppButton size="small" endIcon={<ShareIcon />}>
-											Learn More
-										</AppButton>
-									</>
-								}
-							/>
-						}
-					/>
-				</FadeIn>
-			</Grid>
-		</>
-	);
+  return (
+    <>
+      <Grid container spacing={2}>
+        <StyledFlexBox
+          size={{ xs: 12 }}
+          sx={{
+            textAlign: "center",
+            marginBottom: "100px",
+            marginTop: "100px",
+          }}
+        >
+          <Typography variant="h1">
+            This is the MUI and Motion design system test page (wip) - Scroll
+            down!
+          </Typography>
+        </StyledFlexBox>
+      </Grid>
+      <Grid container spacing={2} sx={{ marginTop: "700px" }}>
+        <StyledFlexBox size={{ xs: 12, md: 6 }}>
+          <StaggerTypography
+            variant="h2"
+            text="This text will stagger in from the pub ;)"
+          />
+        </StyledFlexBox>
+        <StyledFlexBox size={{ xs: 12, md: 6 }}>
+          <FadeIn>
+            <AppButton
+              variant="outlined"
+              color="primary"
+              endIcon={<DeleteIcon />}
+            >
+              Test primary outlined button with icon
+            </AppButton>
+          </FadeIn>
+        </StyledFlexBox>
+      </Grid>
+      <Grid container spacing={2}>
+        <TwoColumnLayout
+          leftColWidth={{
+            xs: "100%",
+            md: "50%",
+          }}
+          rightColWidth={{
+            xs: "100%",
+            md: "50%",
+          }}
+          leftCol={
+            <Typography variant="h2">
+              This is a test 2-col layout component with configurable column
+              widths.
+            </Typography>
+          }
+          rightCol={
+            <FadeIn>
+              <AppButton
+                variant="outlined"
+                color="primary"
+                endIcon={<DeleteIcon />}
+              >
+                Test button
+              </AppButton>
+            </FadeIn>
+          }
+        />
+      </Grid>
+      <Grid container spacing={2}>
+        <TwoColumnLayout
+          leftColWidth={{
+            xs: "100%",
+            md: "100%",
+          }}
+          rightColWidth={{
+            xs: "100%",
+            md: "100%",
+          }}
+          leftCol={
+            <FadeIn>
+              <AppButton
+                variant="outlined"
+                color="primary"
+                endIcon={<ShareIcon />}
+              >
+                Test share button
+              </AppButton>
+            </FadeIn>
+          }
+          rightCol={
+            <FadeIn>
+              <Typography variant="h2">This text will fade in...</Typography>
+            </FadeIn>
+          }
+        />
+      </Grid>
+      <Grid container spacing={2}>
+        <TwoColumnLayout
+          leftColWidth={{
+            xs: "100%",
+            md: "100%",
+          }}
+          rightColWidth={{
+            xs: "100%",
+            md: "100%",
+          }}
+          leftCol={
+            <FadeInBox>
+              <MediaCard
+                title="Test title"
+                variant="primary"
+                description=""
+                image="https://picsum.photos/480/240"
+                actions={
+                  <>
+                    <FadeIn>
+                      <AppButton
+                        variant="outlined"
+                        color="primary"
+                        endIcon={<DeleteIcon />}
+                      >
+                        Test button
+                      </AppButton>
+                    </FadeIn>
+                    <FadeIn>
+                      <AppButton
+                        color="primary"
+                        variant="contained"
+                        endIcon={<ShareIcon />}
+                      >
+                        Share
+                      </AppButton>
+                    </FadeIn>
+                  </>
+                }
+              />
+            </FadeInBox>
+          }
+          rightCol={
+            <FadeInBox>
+              <MediaCard
+                title="Test title"
+                variant="primary"
+                description=""
+                image="https://picsum.photos/480/240"
+                actions={
+                  <>
+                    <FadeIn>
+                      <AppButton
+                        variant="outlined"
+                        color="primary"
+                        endIcon={<DeleteIcon />}
+                      >
+                        Test button
+                      </AppButton>
+                    </FadeIn>
+                    <FadeIn>
+                      <AppButton size="small" endIcon={<ShareIcon />}>
+                        Share
+                      </AppButton>
+                    </FadeIn>
+                  </>
+                }
+              />
+            </FadeInBox>
+          }
+        />
+      </Grid>
+    </>
+  );
 }
--- MUI-Motion-Design_system/src/components/ui/TwoColLayout/TwoColLayout.tsx
+++ MUI-Motion-Design_system/src/components/ui/TwoColLayout/TwoColLayout.tsx
@@ -1,60 +1,61 @@
+"use client";
 import Box from "@mui/material/Box";
 import { ReactNode } from "react";
+import { styled } from "@mui/material/styles";
 
 interface TwoColumnLayoutProps {
-	leftCol?: ReactNode | string;
-	rightCol?: ReactNode | string;
-	leftColWidth?: {
-		xs?: string | number;
-		md?: string | number;
-	};
-	rightColWidth?: {
-		xs?: string | number;
-		md?: string | number;
-	};
+  leftCol?: ReactNode | string;
+  rightCol?: ReactNode | string;
+  leftColWidth?: {
+    xs?: string | number;
+    md?: string | number;
+  };
+  rightColWidth?: {
+    xs?: string | number;
+    md?: string | number;
+  };
 }
 
 export default function TwoColumnLayout({
-	leftCol,
-	rightCol,
-	leftColWidth = { xs: "100%", md: "300px" },
-	rightColWidth = { xs: "100%", md: "1fr" },
+  leftCol,
+  rightCol,
+  leftColWidth = { xs: "100%", md: "300px" },
+  rightColWidth = { xs: "100%", md: "1fr" },
 }: TwoColumnLayoutProps) {
-	return (
-		<Box
-			sx={{
-				display: "flex",
-				gap: 3,
-				width: "100%",
-				flexDirection: {
-					xs: "column",
-					md: "row",
-				},
-			}}
-		>
-			<Box
-				sx={{
-					width: leftColWidth,
-					justifyContent: "center",
-					alignItems: "center",
-					display: "flex",
-					padding: 3,
-				}}
-			>
-				{leftCol}
-			</Box>
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        gap: 3,
+        width: "100%",
+        flexDirection: {
+          xs: "column",
+          md: "row",
+        },
+      }}
+    >
+      <StyledInnerBox
+        sx={{
+          width: leftColWidth,
+        }}
+      >
+        {leftCol}
+      </StyledInnerBox>
 
-			<Box
-				sx={{
-					width: rightColWidth,
-					justifyContent: "center",
-					alignItems: "center",
-					display: "flex",
-					padding: 3,
-				}}
-			>
-				{rightCol}
-			</Box>
-		</Box>
-	);
+      <StyledInnerBox
+        sx={{
+          width: rightColWidth,
+        }}
+      >
+        {rightCol}
+      </StyledInnerBox>
+    </Box>
+  );
 }
+
+export const StyledInnerBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: theme.spacing(2),
+}));
