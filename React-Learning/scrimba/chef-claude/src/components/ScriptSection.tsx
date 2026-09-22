5aa2f5749fea322ed2950844a979625231989f16
Richard Mery <richard.mery@appius.com>
2026-09-22 16:50:18 +0100

    amends to script to scroll with streaming data returned from anthropic

--- React-Learning/scrimba/chef-claude/src/components/ScriptSection.tsx
+++ React-Learning/scrimba/chef-claude/src/components/ScriptSection.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ReactMarkdown from "react-markdown";
+
 type ScriptSectionProps = {
   script: string | null;
 };
 
 const ScriptSection = React.forwardRef<HTMLElement, ScriptSectionProps>(
-  ({ script }, ref) => (
-    <section style={{ marginTop: "20px" }} ref={ref}>
-      <h2>Here's your script:</h2>
-      <article className="suggested-recipe-container" aria-live="polite">
-        <ReactMarkdown>{script || "No script generated yet."}</ReactMarkdown>
-      </article>
-    </section>
-  ),
+  ({ script }, ref) => {
+    const bottomRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      bottomRef.current?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }, [script]);
+
+    return (
+      <section style={{ marginTop: "20px" }} ref={ref}>
+        <h2>Here's your script:</h2>
+        <article className="suggested-recipe-container" aria-live="polite">
+          <ReactMarkdown>{script || "No script generated yet."}</ReactMarkdown>
+          <div ref={bottomRef} />
+        </article>
+      </section>
+    );
+  },
 );
+
 export default ScriptSection;
